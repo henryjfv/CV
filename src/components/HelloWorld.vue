7d2233bbbad07cdcb11069755267c7cff2eb9ce8
henryjfv <henryfernandezv@gmail.com>
2022-03-11 00:07:20 -0500

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,416 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="page-top">
+    <!-- Navigation-->
+    <nav
+      class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
+      id="sideNav"
+    >
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Henry Fernandez</span>
+        <span class="d-none d-lg-block"
+          ><img
+            class="img-fluid img-profile rounded-circle mx-auto mb-2"
+            src="@\assets\img\fotoPerfil.jpg"
+            alt="fotoperfil"
+        /></span>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience"
+              >Experience</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education"
+              >Education</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Certifications</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- Page Content-->
+    <div class="container-fluid p-0">
+      <!-- About-->
+      <section class="resume-section" id="about">
+        <div class="resume-section-content">
+          <h1 class="mb-0">
+            Henry José
+            <span class="text-primary">Fernandez Villarreal</span>
+          </h1>
+          <div class="subheading mb-5">
+            Barranquilla, Atlantico, Colombia
+            <a href="mailto:henryfernandezv@gmail.com"
+              >henryfernandezv@gmail.com</a
+            >
+          </div>
+          <p class="lead mb-5">
+            Systems Engineer, with 5+ years of experience developing software,
+            able to manage Apple Store and Play Store, 2 years developing mobile
+            applications using Android (java), 3+ years developing mobile
+            applications using Flutter (Android and iOS), also developing web
+            applications using Html, Css, Javascript, Vuejs, SQL; using Git as a
+            versioning manager throughout my career, using the Scrum agile
+            development methodology.
+          </p>
+          <div class="social-icons">
+            <a
+              class="social-icon"
+              href="https://www.linkedin.com/in/henryjosefernandez-villarreal/"
+              target="_blank"
+              ><i class="fab fa-linkedin-in"></i
+            ></a>
+            <a
+              class="social-icon"
+              href="https://github.com/henryjfv"
+              target="_blank"
+              ><i class="fab fa-github"></i
+            ></a>
+          </div>
+        </div>
+      </section>
+      <hr class="m-0" />
+      <!-- Experience-->
+      <section class="resume-section" id="experience">
+        <div class="resume-section-content">
+          <h2 class="mb-5">Experience</h2>
+          <div
+            class="d-flex flex-column flex-md-row justify-content-between mb-5"
+          >
+            <div class="flex-grow-1">
+              <h3 class="mb-0">Full Stack Developer</h3>
+              <div class="subheading mb-3">OPENSOLS</div>
+              <p>
+                Web applications development using technologies such as Vuejs,
+                nodejs, Django (python basic), Microsoft .net Framework, and
+                mySQL. Mobile applications development, using Flutter, Xamarin
+                Framework. Manage Play Store and Apple Store.
+              </p>
+            </div>
+            <div class="flex-shrink-0">
+              <span class="text-primary">August 06, 2018 - Currently</span>
+            </div>
+          </div>
+          <div
+            class="d-flex flex-column flex-md-row justify-content-between mb-5"
+          >
+            <div class="flex-grow-1">
+              <h3 class="mb-0">Android Developer</h3>
+              <div class="subheading mb-3">CLOUD TECHNOLOGYS CENTER</div>
+              <p>
+                I made mobile applications, using Java for Android Native
+                applications with Android Studio I did code maintenance for the
+                REST API backend using asp .net Framework, C#, and MySql for the
+                Database.
+              </p>
+            </div>
+            <div class="flex-shrink-0">
+              <span class="text-primary">January 01, 2016 – July 01, 2018</span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr class="m-0" />
+      <!-- Education-->
+      <section class="resume-section" id="education">
+        <div class="resume-section-content">
+          <h2 class="mb-5">Education</h2>
+          <div
+            class="d-flex flex-column flex-md-row justify-content-between mb-5"
+          >
+            <div class="flex-grow-1">
+              <h3 class="mb-0">Universidad Libre seccional Barranquilla</h3>
+              <div class="subheading mb-3">Systems Engineering</div>
+            </div>
+            <div class="flex-shrink-0">
+              <span class="text-primary">Jan 2015 - Nov 2020</span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr class="m-0" />
+      <!-- Skills-->
+      <section class="resume-section" id="skills">
+        <div class="resume-section-content">
+          <h2 class="mb-5">Skills</h2>
+          <div class="subheading mb-3">Programming Languages & Tools</div>
+          <ul class="" style="list-style: none">
+            <li class="">
+              <i class="fab fa-html5"></i>
+              HTML5
+            </li>
+            <li class="">
+              <i class="fab fa-css3-alt"></i>
+              CSS3
+            </li>
+            <li class="">
+              <i class="fab fa-sass"></i>
+              SASS | SCSS
+            </li>
+            <li class="">
+              <i class="fab fa-js-square"></i>
+              JAVASCRIPT
+            </li>
+            <li>
+              <i class="fab fa-vuejs"></i>
+              VUEJS
+            </li>
+            <li class="">
+              <i class="fa fa-mobile-alt"></i>
+              Flutter
+            </li>
+            <li class="">
+              <i class="fab fa-android"></i>
+              Android
+            </li>
+            <li class="">
+              <i class="fab fa-apple"></i>
+              iOS
+            </li>
+            <li class="">
+              <i class="fab fa-python"></i>
+              Django
+            </li>
+            <li class="">
+              <i class="fab fa-java"></i>
+              Java
+            </li>
+            <li class="">
+              <i class="fa fa-database"></i>
+              SQL
+            </li>
+            <li class="">
+              <i class="fab fa-git"></i>
+              GIT
+            </li>
+            <li class="">
+              <i class="fab fa-google-play"></i>
+              Manage PlayStore
+            </li>
+            <li class="">
+              <i class="fab fa-apple"></i>
+              Manage Apple Store
+            </li>
+            <!--             <li class="">
+                <i class="fab fa-angular"></i>
+                ANGULAR
+            </li>
+            <li class="">
+                <i class="fab fa-react"></i>
+                REACT
+            </li> -->
+          </ul>
+          <div class="subheading mb-3">Workflow</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <span class="fa-li"><i class="fas fa-check"></i></span>
+              Requirements Acquisition
+            </li>
+            <li>
+              <span class="fa-li"><i class="fas fa-check"></i></span>
+              Analisys of requirements
+            </li>
+            <li>
+              <span class="fa-li"><i class="fas fa-check"></i></span>
+              Software design and architecture
+            </li>
+            <li>
+              <span class="fa-li"><i class="fas fa-check"></i></span>
+              Agile Development & Scrum
+            </li>
+            <li>
+              <span class="fa-li"><i class="fas fa-check"></i></span>
+              Define Stories, Tasks, Backlog, sprints, ...
+            </li>
+            <li>
+              <span class="fa-li"><i class="fas fa-check"></i></span>
+              The development stage begins
+            </li>
+            <li>
+              <span class="fa-li"><i class="fas fa-check"></i></span>
+              Development tests
+            </li>
+            <li>
+              <span class="fa-li"><i class="fas fa-check"></i></span>
+              Product deployment
+            </li>
+          </ul>
+        </div>
+      </section>
+      <hr class="m-0" />
+      <!-- Awards-->
+      <section class="resume-section" id="awards">
+        <div class="resume-section-content">
+          <h2 class="mb-5">Certifications</h2>
+
+          <div
+            id="carouselExampleIndicators"
+            class="carousel slide"
+            data-bs-ride="carousel"
+            style="background-color: #b7b7b7"
+          >
+            <div class="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="0"
+                class="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+         <!--      <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button> -->
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  src="@\assets\img\cert1.png"
+                  class="d-block w-100"
+                  style="height: 40rem; object-fit: scale-down"
+                  alt="..."
+                />
+              </div>
+<!--               <div class="carousel-item">
+                <img
+                  src="@\assets\img\fotoPerfil.jpg"
+                  class="d-block w-100"
+                  style="height: 40rem; object-fit: scale-down"
+                  alt="..."
+                />
+              </div> -->
+              <div class="carousel-item">
+                <img
+                  src="@\assets\img\cert2.png"
+                  class="d-block w-100"
+                  style="height: 40rem; object-fit: scale-down"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+          <!--           <ul class="fa-ul mb-0">
+            <li>
+              <span class="fa-li"
+                ><i class="fas fa-trophy text-warning"></i
+              ></span>
+              Google Analytics Certified Developer
+            </li>
+            <li>
+              <span class="fa-li"
+                ><i class="fas fa-trophy text-warning"></i
+              ></span>
+              Mobile Web Specialist - Google Certification
+            </li>
+            <li>
+              <span class="fa-li"
+                ><i class="fas fa-trophy text-warning"></i
+              ></span>
+              1
+              <sup>st</sup>
+              Place - University of Colorado Boulder - Emerging Tech Competition
+              2009
+            </li>
+            <li>
+              <span class="fa-li"
+                ><i class="fas fa-trophy text-warning"></i
+              ></span>
+              1
+              <sup>st</sup>
+              Place - University of Colorado Boulder - Adobe Creative Jam 2008
+              (UI Design Category)
+            </li>
+            <li>
+              <span class="fa-li"
+                ><i class="fas fa-trophy text-warning"></i
+              ></span>
+              2
+              <sup>nd</sup>
+              Place - University of Colorado Boulder - Emerging Tech Competition
+              2008
+            </li>
+            <li>
+              <span class="fa-li"
+                ><i class="fas fa-trophy text-warning"></i
+              ></span>
+              1
+              <sup>st</sup>
+              Place - James Buchanan High School - Hackathon 2006
+            </li>
+            <li>
+              <span class="fa-li"
+                ><i class="fas fa-trophy text-warning"></i
+              ></span>
+              3
+              <sup>rd</sup>
+              Place - James Buchanan High School - Hackathon 2005
+            </li>
+          </ul> -->
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/js/scripts.js'
 
 createApp(App).mount('#app')
